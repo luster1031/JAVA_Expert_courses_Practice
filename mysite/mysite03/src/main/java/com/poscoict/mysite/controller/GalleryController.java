2448037946a429ab2a04a50f554cc38d6cfc48dc
luster1031 <luster47@naver.com>
2022-02-18 17:01:48 +0900

    [mysite03] ADD gallery

---
+++ mysite/mysite03/src/main/java/com/poscoict/mysite/controller/GalleryController.java
@@ -0,0 +1,52 @@
+package com.poscoict.mysite.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.poscoict.mysite.security.Auth;
+import com.poscoict.mysite.service.FileUploadService;
+import com.poscoict.mysite.service.GalleryService;
+
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+	@Autowired
+	private FileUploadService fileUploadService;
+	
+	@Autowired
+	private GalleryService galleryService;
+	
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		galleryService.getImages(model);
+		return "gallery/index";
+	}
+	
+	
+	@Auth(role="ADMIN")
+	@RequestMapping("/delete/{no}")
+	public String delete(@PathVariable("no") Long no) {
+		galleryService.removeImage(no);
+		System.out.println("delete gallery :" + no);
+		return "redirect:/gallery";
+	}
+	
+	@RequestMapping(value="/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("file") MultipartFile multipartFile
+			, @RequestParam(value="comments"
+				, required = true
+				, defaultValue = "") String comments) {
+		String url = fileUploadService.restore(multipartFile);
+		galleryService.saveImage(url, comments);
+		return "redirect:/gallery";
+	}
+}
