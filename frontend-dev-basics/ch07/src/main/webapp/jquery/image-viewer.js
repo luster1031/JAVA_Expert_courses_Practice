5c0e5659da79ac3d9a797df3af3ed4f7cc100ecf
luster1031 <luster47@naver.com>
2022-02-25 21:24:09 +0900

    [ch07] slideshow

--- frontend-dev-basics/ch07/src/main/webapp/jquery/image-viewer.js
+++ frontend-dev-basics/ch07/src/main/webapp/jquery/image-viewer.js
@@ -1,6 +1,58 @@
+
 var imageViewer ={
 	init:function(){
-		var images = [ 
+		console.log(this);
+		$(function() {
+			console.log(this);
+			var interval = null;
+			var isStop = false;
+			$("#btn-change").click(function() {
+				this._changeImage;
+				$(".image-viewer #btn-slideshow").text('슬라이드쇼 중지');
+			});
+			$("img").click(function() {
+				this._changeImage.bind(this);
+				$(".image-viewer #btn-slideshow").text('슬라이드쇼 중지');
+			});
+			$("#btn-slideshow").click(function() {
+				
+				if(!isStop){
+					interval = setInterval(function(){
+						console.log(typeof(this._changeImage));
+						console.log(this);
+						this._changeImage();
+					}.bind(this),1000);
+					$(".image-viewer #btn-slideshow").text('슬라이드쇼 중지');
+					console.log(isStop);
+					isStop = true;
+				}
+				else{
+					console.log("중지");
+					clearInterval(interval);
+					$(".image-viewer #btn-slideshow").text('슬라이드쇼 시작');
+					isStop = false;
+				}
+			}.bind(this));
+		}.bind(this));
+		
+	},
+	_changeImage:function() {
+		
+		console.log(this);
+		
+		console.log("이미지바꾸기");
+		console.log(this.setInterval);
+		
+		var index = Math.floor(Math.random() * (this._images.length - 1)) + 1;
+		$(".image-viewer img").attr({
+			alt : this._images[index].name,
+			src : 'images/' + this._images[index].file
+		});
+		
+	},
+	//	추가
+	
+	_images : [ 
 	    {
 	    	name:"국화",
 	    	file:"Chrysanthemum.jpg"
@@ -32,10 +84,5 @@ var imageViewer ={
 	    {
 	   	 	name:"툴립",
 	   	 	file:"Tulips.jpg"  
-	    }];
-
-	}
-	//	추가
-	
-	
+	    }]
 }
