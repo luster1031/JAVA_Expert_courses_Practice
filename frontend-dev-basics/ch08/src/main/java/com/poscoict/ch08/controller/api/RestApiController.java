b6f25f4c532536a5a822caf4247ea72a2809c434
luster1031 <luster47@naver.com>
2022-02-28 12:56:00 +0900

    [ch08] ajax #7 - @GetMapping, @PutMapping, @DeleteMapping

--- frontend-dev-basics/ch08/src/main/java/com/poscoict/ch08/controller/api/RestApiController.java
+++ frontend-dev-basics/ch08/src/main/java/com/poscoict/ch08/controller/api/RestApiController.java
@@ -1,8 +1,16 @@
 package com.poscoict.ch08.controller.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poscoict.ch08.controller.dao.JsonResult;
@@ -19,5 +27,66 @@ public class RestApiController {
 		vo.setPassword("");	//	password는 교환 많이 하는게 안 좋음
 		
 		return JsonResult.success(vo);
+	}
+	
+	@GetMapping("")	//	다 가져와라	- 관리자 프로그램 만들 때 필요
+	public Object read() {
+		List<UserVo> list = new ArrayList<>();
+		
+		UserVo vo1 = new UserVo();
+		vo1.setNo(1L);
+		vo1.setName("둘리");
+		list.add(vo1);
+		
+		UserVo vo2 = new UserVo();
+		vo2.setNo(2L);
+		vo2.setName("마이콜");
+		list.add(vo2);
+		return JsonResult.success(list);
+	}
+	
+	@GetMapping("/{no}")	//	다 가져와라	- 관리자 프로그램 만들 때 필요
+	public Object read(@PathVariable("no") Long no) {
+		System.out.println("no : " + no);
+		
+		UserVo vo2 = new UserVo();
+		vo2.setNo(2L);
+		vo2.setName("마이콜");
+		
+		return JsonResult.success(vo2);
+	}
+	
+	//	@Auth
+	//	authuser no 와 uservo의 no 같은지 확인해봐야한다.
+	@PutMapping("/{no}")	//	다 가져와라	- 관리자 프로그램 만들 때 필요
+	public Object update(@PathVariable("no") Long no,  @RequestBody UserVo vo /*@AuthUser UserVo authUser*/) {
+		System.out.println("[PUT no] : " + no);
+		System.out.println("[PUT vo] : " + vo.toString());
+		vo.setNo(no);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	}
+	
+	@DeleteMapping("/{no}")
+	public Object delete(
+			@PathVariable("no") Long no
+			, @RequestParam(value="password", required = true, defaultValue = "")String password) {
+		
+		System.out.println("[delete no] : " + no);
+		System.out.println("[delete password] : " + password);
+		/*
+		 delete는 톰캣이 해줘야하기 때문에
+		  server.xml에 delete 설정해줘야한다.
+		  <Connector 
+		    URIEncoding="UTF-8"
+		    parseBodyMethods="POST, PUT, DELETE"
+		    connectionTimeout="20000" 
+			port="8080" 
+			protocol="HTTP/1.1" 
+			redirectPort="8443"
+			/>
+		 */
+		return JsonResult.success(no);
 	}
 }
--- frontend-dev-basics/ch08/src/main/webapp/WEB-INF/views/rest-test.jsp
+++ frontend-dev-basics/ch08/src/main/webapp/WEB-INF/views/rest-test.jsp
@@ -11,7 +11,74 @@
 <script type="text/javascript" src="${pageContext.request.contextPath }/jquery/jquery-3.6.0.js"></script>
 <script>
 $(function(){
-	
+	$("#create").click(function(){
+		var vo={
+				name:"둘리"
+				,email:"dooly@gmail.com"
+				,password:"1234"
+				,gender:"male"
+		}
+		$.ajax({
+			url:"${pageContext.request.contextPath }/api/user"
+			,async:true
+			,type:'post'		//	요청 method
+			,dataType:"json"	//	응답 포맷
+			,contentType:'application/json'	//	default	아님
+			,data:JSON.stringify(vo)		//	string으로 보내줘야함
+			,success:function(response/*응답된 객체*/){
+				console.log(response);
+			}
+		});
+	});
+	$("#read").click(function(){
+		$.ajax({
+			url:"${pageContext.request.contextPath }/api/user/10"
+			,async:true
+			,type:'get'			//	요청 method
+			,dataType:"json"	//	응답 포맷
+			
+			//	body가 없어서
+			//	,contentType:'application/json'	
+			//	,data:JSON.stringify(vo)		//	string으로 보내줘야함
+			
+			,success:function(response/*응답된 객체*/){
+				console.log(response);
+			}
+		});
+	});
+	$("#update").click(function(){
+		var vo={
+				name:"둘리"
+				,email:"dooly@gmail.com"
+				,password:"1234"
+				,gender:"fmale"
+		}
+		$.ajax({
+			url:"${pageContext.request.contextPath }/api/user/10"
+			,async:true
+			,type:'put'			//	요청 method
+			,dataType:"json"	//	응답 포맷
+			,contentType:'application/json'	
+			,data:JSON.stringify(vo)		//	string으로 보내줘야함
+			,success:function(response/*응답된 객체*/){
+				console.log(response);
+			}
+		});
+	});
+	$("#delete").click(function(){
+		
+		$.ajax({
+			url:"${pageContext.request.contextPath }/api/user/10"
+			,async:true
+			,type:'delete'			//	요청 method
+			,dataType:"json"		//	응답 포맷
+			,contentType:'application/x-www-form-urlencoded'	
+			,data:"password=1234"		
+			,success:function(response/*응답된 객체*/){
+				console.log(response);
+			}
+		});
+	});
 });
 </script>
 </head>
