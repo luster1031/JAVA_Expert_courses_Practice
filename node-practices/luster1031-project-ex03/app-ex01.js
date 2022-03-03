22b0567bdf6cc026c3fa9384fb579db8a4720151
luster1031 <luster47@naver.com>
2022-03-03 21:39:38 +0900

    npm registry 원격 배포 실습

---
+++ node-practices/luster1031-project-ex03/app-ex01.js
@@ -0,0 +1,5 @@
+const lusterMath = require('posco-ict-luster1031-math');
+
+console.log(lusterMath.sum(10, 20, 30, 40, 50));
+console.log(lusterMath.max(-10, -20, -30));
+console.log(lusterMath.min(-30, -20, -10, 0, 10, 20, 30));
