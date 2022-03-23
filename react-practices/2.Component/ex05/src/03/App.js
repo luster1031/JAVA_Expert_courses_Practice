85926377860bbb2ea97cd45b5c1118d25d495ea1
luster1031 <luster47@naver.com>
2022-03-23 11:02:20 +0900

    lifecycle #3

--- react-practices/2.Component/ex05/src/03/App.js
+++ react-practices/2.Component/ex05/src/03/App.js
@@ -5,20 +5,21 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor() {
         super(...arguments);
-
         this.state = this.getCurrentClockTime();
 
     }
 
-    getCurrentClockTime(){
+    getCurrentClockTime() {
         const date = new Date();
-        const hours =date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        const session = hours > 12 ? 'am' : 'pm';
+        const hours = date.getHours();
+
 
         return {
-            
+            hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2)
+            , minutes: ('0' + date.getMinutes()).slice(-2)
+            , seconds: ('0' + date.getSeconds()).slice(-2)
+            , session: hours > 12 ? 'am' : 'pm'
+            , ticks: this.state ? this.state.ticks + 1 : 0     
         };
     }
 
@@ -26,12 +27,28 @@ export default class App extends Component {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock 
-                        hours={10}
-                        minutes={20}
-                        seconds={40}
-                        session={'am'}/>
+                {
+                    this.state.ticks % 10 == 0 ?
+                        null : 
+                        <Clock
+                            hours={this.state.hours}
+                            minutes={this.state.minutes}
+                            seconds={this.state.seconds}
+                            session={this.state.session} />
+                    
+
+                }
             </div>
         );
+    }
+    componentDidMount() {
+        this.interval = setInterval(function () {
+            this.setState(this.getCurrentClockTime());
+        }.bind(this), 1000);
+    }
+    
+    componentWillUnmount(){
+        console.log("unmounte!!!!");
+        clearInterval(this.interval);
     }
 }
