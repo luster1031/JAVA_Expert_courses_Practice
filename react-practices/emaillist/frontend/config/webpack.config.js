2939d2da5215f1c624c70be8776175fd4cb297d2
luster1031 <luster47@naver.com>
2022-03-23 21:19:52 +0900

    emaillist init

--- react-practices/emaillist/frontend/config/webpack.config.js
+++ react-practices/emaillist/frontend/config/webpack.config.js
@@ -39,6 +39,9 @@ module.exports = function(env) {
         devServer:{
             host:'0.0.0.0', //  아무대나 들어올 수 있음
             port:9090,
+            Proxy:{
+                '/api' : 'http://localhost:8080'    //  특정 url을 특정 서버로
+            },
             liveReload:true, //  소스 변경됐을 때, 찾아서 서버 다시 올릴 수 있음
             hot :false,
             compress:true,    //  public의 내용을 서버 메모리에 올려서 작업함
--- react-practices/emaillist/frontend/src/App.js
+++ react-practices/emaillist/frontend/src/App.js
@@ -4,10 +4,24 @@ import RegisterFom from './RegisterFom'
 import Searchbar from './Searchbar'
 import Emaillist from './Emaillist'
 import data from './assets/json/data.json'
+import { useEffect } from 'react/cjs/react.production.min'
 
 const App = () => {
   const [emails, setEmails]= useState(data);
   const [keyword, setKeyword] = useState('');
+
+
+  useEffect(async ()=>{
+    const response = await fetch('/api',{
+      method:'get',
+      headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      },
+      body:null
+    });
+    console.log(response);
+  },[]);
 
   const notifyKeywordChange = function(keyword) {
     setEmails(data.filter(e => e.firstName.indexOf(keyword) != -1 || e.lastName.indexOf(keyword) != -1 || e.email.indexOf(keyword) != -1));
