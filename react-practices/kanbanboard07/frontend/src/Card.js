948cc8d74086fb7a1fc302758e1e82297c7ff483
luster1031 <luster47@naver.com>
2022-03-28 12:23:02 +0900

    [frontend #2]

--- react-practices/kanbanboard07/frontend/src/Card.js
+++ react-practices/kanbanboard07/frontend/src/Card.js
@@ -1,16 +1,26 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import TaskList from './TaskList'
 import style from './assets/css/Card.css'
 
-const Card = ({cardNo, card})  => {
+const Card = ({ cardNo, card }) => {
   const [showDetails, setShowDetails] = useState(false);
 
-  
+  const toggleClass = () => {
+    setShowDetails(!showDetails);
+  }
   return (
     <div className={style.Card}>
-      <div className={style.Card__Title} >{card.title}</div>
-      <div className={style.Card__Details}>{card.description}</div>
-      <TaskList tasks={card.tasks}/>
+      <div className={showDetails ? [style.Card__Title, style.Card__Title__open].join(' ') : style.Card__Title}
+        onClick={toggleClass}>
+        {card.title}</div>
+      {
+        showDetails ?
+          <div className={style.Card__Details}>
+            {card.description}
+            <TaskList tasks={card.tasks} />
+          </div> : null
+      }
+
     </div>
   )
 }
